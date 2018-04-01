4cdae55b1638453f2c0c2b14e7be4a051aec47aa
Parkat13 <katye96@mail.ru>
2018-04-02 00:33:28 +0300

    Add files via upload

--- src/Rang.py
+++ src/Rang.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
-import sys
 import codecs
-reload(sys)
-sys.setdefaultencoding('utf-8')
-f = codecs.open('resC-Value.txt', 'r', 'utf8')
+f = codecs.open('second_resWordsWord2vec.txt', 'r', 'utf8')
 f_g = codecs.open('second_phrases.txt', 'r', 'utf8')
-f_t = open('TRangC-Value.txt', 'w')
-f_n = open('NRangC-Value.txt', 'w')
+f_t = open('TRangWordsWord2vec.txt', 'w')
+f_n = open('NRangWordsWord2vec.txt', 'w')
 a = f.read().split()
 b = f_g.read().split()
 dict = {}
-for i in range(len(b)/4):
+for i in range(len(b)//4):
     dict[b[i*4 +1] + ' ' + b[i*4 + 2]] = b[i*4]
 n = 1
-for i in range(len(a)/3):
-    if dict[a[i*3] + ' ' + a[i*3 + 1]] == 'T':
-        f_t.write(a[i * 3] + ' ' + a[i * 3 + 1] + ' ' + str(n) + ' ' + 'C-Value' + '\n')
+for i in range(len(a)//4):
+    if dict[a[i*4+1] + ' ' + a[i*4 + 2]] == 'T':
+        f_t.write(a[i * 4+1] + ' ' + a[i * 4 + 2] + ' ' + str(n) + ' ' + 'WordsWord2vec' + '\n')
     else:
-        f_n.write(a[i * 3] + ' ' + a[i * 3 + 1] + ' ' + str(n) + ' ' + 'C-Value' + '\n')
+        f_n.write(a[i * 4+1] + ' ' + a[i * 4 + 2] + ' ' + str(n) + ' ' + 'WordsWord2vec' + '\n')
     n += 1
 f.close()
 f_g.close()
