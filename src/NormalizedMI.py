2187953f063beb86f25f59e1ebcb01e82f65357a
Parkat13 <katye96@mail.ru>
2018-06-08 02:17:33 +0300

    Add files via upload

--- src/NormalizedMI.py
+++ src/NormalizedMI.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-import sys
 import codecs
 import math
-reload(sys)
-sys.setdefaultencoding('utf-8')
 w = 446134717
 f_freq = codecs.open('second_phrases.txt', 'r', 'utf8')
 f_tf = codecs.open('secondTFWords.txt', 'r', 'utf8')
@@ -11,7 +8,7 @@ f_res = open('second_resNormalizedMI.txt', 'w')
 a = f_freq.read().split()
 tf = f_tf.read().split()
 dict = {}
-for i in range(len(a) / 4):
+for i in range(len(a) // 4):
     dict[a[i * 4] + ' ' + a[i * 4 + 1] + ' ' + a[i * 4 + 2]] = math.log(1.0 * w * float(a[i * 4 + 3]) / (float(tf[i * 4 + 2]) * float (tf[i * 4 + 3]))) / (- math.log(1.0 * float(a[i * 4 + 3]) / w))
 for i in sorted(dict, key=dict.__getitem__, reverse=True):
     f_res.write(str(i) + ' ' + str(dict[i]) + '\n')
