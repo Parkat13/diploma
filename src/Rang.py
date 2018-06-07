d993efd93615d3a455f7d0503e7168b2d9dbac5a
Parkat13 <katye96@mail.ru>
2018-06-08 02:25:34 +0300

    Add files via upload

--- src/Rang.py
+++ src/Rang.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import codecs
-f = codecs.open('second_resWordsWord2vec.txt', 'r', 'utf8')
+f = codecs.open('second_resSinonimFinish(only_T).txt', 'r', 'utf8')
 f_g = codecs.open('second_phrases.txt', 'r', 'utf8')
-f_t = open('TRangWordsWord2vec.txt', 'w')
-f_n = open('NRangWordsWord2vec.txt', 'w')
+f_t = open('TRangSinonim(only_T).txt', 'w')
+f_n = open('NRangSinonim(only_T).txt', 'w')
 a = f.read().split()
 b = f_g.read().split()
 dict = {}
@@ -12,9 +12,9 @@ for i in range(len(b)//4):
 n = 1
 for i in range(len(a)//4):
     if dict[a[i*4+1] + ' ' + a[i*4 + 2]] == 'T':
-        f_t.write(a[i * 4+1] + ' ' + a[i * 4 + 2] + ' ' + str(n) + ' ' + 'WordsWord2vec' + '\n')
+        f_t.write(a[i * 4+1] + ' ' + a[i * 4 + 2] + ' ' + str(n) + ' ' + 'Sinonim(only_T)' + '\n')
     else:
-        f_n.write(a[i * 4+1] + ' ' + a[i * 4 + 2] + ' ' + str(n) + ' ' + 'WordsWord2vec' + '\n')
+        f_n.write(a[i * 4+1] + ' ' + a[i * 4 + 2] + ' ' + str(n) + ' ' + 'Sinonim(only_T)' + '\n')
     n += 1
 f.close()
 f_g.close()
