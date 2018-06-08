8eeb1b0021985eea8cc5611c255d6c26be17812e
Parkat13 <katye96@mail.ru>
2018-06-08 03:07:32 +0300

    Add files via upload

--- src/Sinonim.py
+++ src/Sinonim.py
@@ -1,21 +1,74 @@
 # -*- coding: utf-8 -*-
-import sys
 from gensim.models.keyedvectors import KeyedVectors
 model = KeyedVectors.load_word2vec_format('text_output.txt', binary=False)
 import codecs
 f_ph = codecs.open('second_phrases.txt', 'r', 'utf8')
-f_res = open('second_resSinonim.txt', 'w')
+f_pff = codecs.open('phrases(200,250).txt', 'r', 'utf8')
+f_res = open('min_resSinonim(only_T).txt', 'w')
+f_data = codecs.open('text_output.txt', 'r', 'utf8')
+f_tes = codecs.open('tesaurus.txt', 'r', 'utf8')
+a = f_data.readline().split()
+d = {}
+while len(a) > 0:
+    d[a[0]] = 0
+    a = f_data.readline().split()
 print('1\n')
 group = f_ph.read().split()
+tes_dict = {}
+for i in range(len(group)//4):
+    if group[4*i] == 'T' and int(group[4*i+3]) > 250:
+        tes_dict[group[4*i+1] + '_' + group[4*i+2]] = group[4*i]
+tes_word = {}
+tes = f_tes.readline().lower().split()
+while len(tes) > 0:
+    if len(tes) == 1:
+        tes_word[tes[0]] = 'T'
+    tes = f_tes.readline().lower().split()
 dict = {}
-for i in range(int(len(group)/4)):
+group = f_pff.read().split()
+for i in range(len(group)//4):
     most_sim = model.wv.most_similar(positive=[group[4 * i + 1] + '_' + group[4 * i + 2]], negative=[], topn=1)
     k, j = 1, 0
-    while ('_' in most_sim[j][0]) or not(most_sim[j][0].isalpha()) or (group[4 * i + 1] == most_sim[j][0]) or (group[4 * i + 2] == most_sim[j][0]):
+    ##mas = most_sim[j][0].split('_')
+    ##ob = []
+    ##flag = 0
+    ##if len(mas) > 1:
+        ##flag = 0
+        ##if group[4*i+1] == mas[0]:
+          ##  flag = 1
+            ##ob = [group[4*i+2], mas[1]]
+        ##if group[4 * i + 2] == mas[0]:
+          ##  flag = 1
+            ##ob = [group[4 * i + 1], mas[1]]
+        ##if group[4 * i + 1] == mas[1]:
+          ##  flag = 1
+            ##ob = [group[4 * i + 2], mas[0]]
+        ##if group[4 * i + 2] == mas[1]:
+          ##  flag = 1
+            ##ob = [group[4 * i + 1], mas[0]]
+
+
+    while (not ('_' in most_sim[j][0]) and not (most_sim[j][0] in tes_word)) or (group[4 * i + 1] == most_sim[j][0]) or (group[4 * i + 2] == most_sim[j][0]) or (('_' in most_sim[j][0]) and not (most_sim[j][0] in tes_dict)):
         if k == (j + 1):
             k += 5
             most_sim = model.wv.most_similar(positive=[group[4 * i + 1] + '_' + group[4 * i + 2]], negative=[], topn=k)
         j += 1
+        ##mas = most_sim[j][0].split('_')
+        ##ob = []
+        ##if len(mas) > 1:
+          ##  flag = 0
+            ##if group[4 * i + 1] == mas[0]:
+              ##  flag = 1
+                ##ob = [group[4 * i + 2], mas[1]]
+            ##if group[4 * i + 2] == mas[0]:
+              ##  flag = 1
+                ##ob = [group[4 * i + 1], mas[1]]
+            ##if group[4 * i + 1] == mas[1]:
+              ##  flag = 1
+                ##ob = [group[4 * i + 2], mas[0]]
+            ##if group[4 * i + 2] == mas[1]:
+              ##  flag = 1
+                ##ob = [group[4 * i + 1], mas[0]]
     dict[group[4*i] + ' ' + group[4 * i + 1] + '_' + group[4 * i + 2] + ' ' + most_sim[j][0]] = most_sim[j][1]
     
 print('2\n')
@@ -23,3 +76,4 @@ for i in sorted(dict, key=dict.__getitem__, reverse=True):
     f_res.write(str(i) + ' ' + str(dict[i]) + '\n')
 f_ph.close()
 f_res.close()
+f_data.close()
--- src/SinonimFinish.py
+++ src/SinonimFinish.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-import sys
 import codecs
-reload(sys)
-sys.setdefaultencoding('utf-8')
-f = codecs.open('second_resSinonim.txt', 'r', 'utf8')
-f_t = open('second_resSinonimFinish.txt', 'w')
+f = codecs.open('min_resSinonim(only_T).txt', 'r', 'utf8')
+f_t = open('min_resSinonimFinish(only_T).txt', 'w')
 a = f.read().split()
 
-for i in range(len(a)/4):
+for i in range(len(a)//4):
     b = a[i*4 + 1].split('_')
     f_t.write(a[i * 4] + ' ' + b[0] + ' ' + b[1] + ' ' + a[i*4 + 3] + '\n')
 f.close()
